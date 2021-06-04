deec7663ea1c5b77e918b021c00cf7ed8184ff5d
aPailOfCoffee <bshestakoff@gmail.com>
2021-06-04 21:21:50 +0300

    1. Модульные тесты. [#159435]

---
+++ test/ru/job4j/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.ru.job4j.converter;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenConvert600RbkThen10Dlr() {
+        int in = 600;
+        int expected = 10;
+        int out = Converter.rubleToDollar(in);
+        Assert.assertEquals(expected, out);
+    }
+}
