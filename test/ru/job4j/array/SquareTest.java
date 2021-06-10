fa5caac01cf04ef8aa3a05c9d5a90b5b2089dd0d
aPailOfCoffee <bshestakoff@gmail.com>
2021-06-10 20:49:55 +0300

    6.0.5. Массивы в тестах. [#226789] secondTestNameChanged

--- test/ru/job4j/array/SquareTest.java
+++ test/ru/job4j/array/SquareTest.java
@@ -15,7 +15,7 @@ public class SquareTest {
     }
 
     @Test
-    public void whenBound5Then014() {
+    public void whenBound5Then0149And16() {
         int bound = 5;
         int[] result = Square.calculate(bound);
         int[] expected = new int[] {0, 1, 4, 9, 16};
