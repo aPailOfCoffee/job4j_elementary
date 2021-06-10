79df7dfa4b1ffeb078bf1f38fa0fd48528fad636
aPailOfCoffee <bshestakoff@gmail.com>
2021-06-10 10:45:51 +0300

    5.7. Ипотека [#156315 #166361]

--- src/ru/job4j/loop/MortGage.java
+++ src/ru/job4j/loop/MortGage.java
@@ -3,8 +3,9 @@ package ru.job4j.loop;
 public class MortGage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
+        percent /= 100;
         while (amount > 0) {
-            amount = amount + amount * percent / 100 - salary;
+            amount = amount + amount * percent - salary;
             year++;
         }
         return year;
